1573c8ebb64aaab1a68037c319aba704d259355a
mumen345 <kingxosas@gmail.com>
2023-05-10 21:55:22 +0100

    changes

--- src/main.js
+++ src/main.js
@@ -3,13 +3,5 @@ import { createApp } from 'vue'
 import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
 import router from './router'
-//theme
-import "primevue/resources/themes/lara-light-indigo/theme.css";
-
-//core
-import "primevue/resources/primevue.min.css";
-import ToastService from 'primevue/toastservice';
-// import Toasted from 'vue-toasted';
 import store from './store'
-import PrimeVue from 'primevue/config';
-createApp(App).use(store).use(PrimeVue).use(ToastService).use(MotionPlugin).use(router).mount('#app')
+createApp(App).use(store).use(MotionPlugin).use(router).mount('#app')
